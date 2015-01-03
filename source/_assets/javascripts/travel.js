aa14bf3b1644f42f3d0291f519d91c1c9220639d
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-01-03 16:43:22 +0100

    Fixed broken travel map.

--- source/_assets/javascripts/travel.js
+++ source/_assets/javascripts/travel.js
@@ -1,51 +1,77 @@
+(function() {
+
+    var projection = d3.geo.winkel3();
 
 
-
-        var map = new Datamap({
-            element: document.querySelector('.travel-map'),
-            scope: 'world',
-            setProjection: function(element, options) {
-                var projection = d3.geo.winkel3()
-                .scale(180)
+    var map = new Datamap({
+        element: document.querySelector('.travel-map'),
+        scope: 'world',
+        disableDefaultStyles: true,
+        setProjection: function(element, options) {
+            
+            projection = d3.geo.naturalEarth()
+                .scale(240)
                 .translate([element.offsetWidth / 2, element.offsetHeight / 2])
-                .precision(.1);
 
-                path = d3.geo.path()
-                    .projection( projection );
+            var path = d3.geo.path()
+                .projection( projection );
 
-                return {path: path, projection: projection};
-            },
-            fills: {
-                defaultFill: "#F5F5F5",
-                beenTo: "#2B2D34"
-            },
-            geographyConfig: {
-                hideAntarctica: true,
-                borderWidth: 1,
-                borderColor: '#CCCCCC',
-                popupOnHover: false,
-                highlightOnHover: true,
-                highlightFillColor: '#FC8D59',
-                highlightBorderColor: 'rgba(250, 15, 160, 0.2)',
-                highlightBorderWidth: 2
-            },            
-            data: {
-                DNK: { fillKey: "beenTo" },
-                USA: { fillKey: "beenTo" },
-                DEU: { fillKey: "beenTo" },
-                ITA: { fillKey: "beenTo" },
-                FRA: { fillKey: "beenTo" },
-                ESP: { fillKey: "beenTo" },
-                THA: { fillKey: "beenTo" },
-                MYS: { fillKey: "beenTo" },
-                QAT: { fillKey: "beenTo" },
-                NLD: { fillKey: "beenTo" },
-                SWE: { fillKey: "beenTo" },
-                NOR: { fillKey: "beenTo" },
-                GBR: { fillKey: "beenTo" },
-                AUT: { fillKey: "beenTo" },
-                ROU: { fillKey: "beenTo" },
-                CZE: { fillKey: "beenTo" },
-                GRC: { fillKey: "beenTo" },
-            }            
-        });
+            return {path: path, projection: projection};
+        },
+        fills: {
+            defaultFill: "#F5F5F5",
+            beenTo: "#2B2D34",
+            home: "#FFA000",
+            planned: "#3F51B5"
+        },
+        geographyConfig: {
+            hideAntarctica: true,
+            borderWidth: 1,
+            borderColor: '#CCCCCC',
+            popupOnHover: true,
+            highlightOnHover: true,
+            highlightFillColor: '#FC8D59',
+            highlightBorderColor: 'rgba(250, 15, 160, 0.2)',
+            highlightBorderWidth: 2
+        },            
+        data: {
+            DNK: { fillKey: "home" },
+            USA: { fillKey: "beenTo" },
+            DEU: { fillKey: "beenTo" },
+            ITA: { fillKey: "beenTo" },
+            FRA: { fillKey: "beenTo" },
+            ESP: { fillKey: "beenTo" },
+            THA: { fillKey: "beenTo" },
+            MYS: { fillKey: "beenTo" },
+            QAT: { fillKey: "beenTo" },
+            NLD: { fillKey: "beenTo" },
+            SWE: { fillKey: "beenTo" },
+            NOR: { fillKey: "beenTo" },
+            GBR: { fillKey: "beenTo" },
+            AUT: { fillKey: "beenTo" },
+            ROU: { fillKey: "beenTo" },
+            CZE: { fillKey: "beenTo" },
+            GRC: { fillKey: "beenTo" },
+            VNM: { fillKey: "planned" },
+            LAO: { fillKey: "planned" },
+            MMR: { fillKey: "planned" },
+            IDN: { fillKey: "planned" },
+            PHL: { fillKey: "planned" },
+            TLS: { fillKey: "planned" },
+            PNG: { fillKey: "planned" },
+        }            
+    });
+
+    window.addEventListener('resize', function() {
+        var elmMap = document.querySelector('.travel-map > svg');
+        var elmContainer = document.querySelector('.travel-map');
+        
+        var targetWidth = elmContainer.offsetWidth;
+        var aspect = elmMap.offsetWidth / elmMap.offsetHeight;
+
+        d3.select(elmMap).attr('width', targetWidth);
+        d3.select(elmMap).attr('height', targetWidth / aspect);
+
+    }, false);
+
+})();
